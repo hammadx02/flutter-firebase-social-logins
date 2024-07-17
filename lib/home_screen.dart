1ebd85313ce1d7056e462356b1c9f443e71475a6
hammadx02 <hammadaali02@gmail.com>
2024-07-17 15:29:09 +0500

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,22 +12,62 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'Social Logins',
-            style: GoogleFonts.roboto(
-              textStyle: const TextStyle(
-                fontSize: 24,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Social Logins',
+              style: GoogleFonts.roboto(
+                textStyle: const TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 54,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color(0XFF1877F2),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.084),
+                      blurRadius: 3,
+                      spreadRadius: 0,
+                      offset: const Offset(0, 0),
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  leading: Image.asset(
+                    'assets/images/fb.png',
+                    scale: 3.3,
+                  ),
+                  title: Text(
+                    'Sign In with Facebook',
+                    style: GoogleFonts.inter(
+                      textStyle: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
